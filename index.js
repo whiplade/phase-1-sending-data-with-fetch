e204190547039a9ddf0a660f39eb1b03b3ed73e7
whiplade <nmawira13@gmail.com>
2023-06-26 15:09:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+const formData = {
+    name: 'Steve',
+    email: 'steve@steve.com',
+}
+
+function submitData() {
+
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {"Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData)
+    })
+
+    .then(res => res.json())
+    .then(object => {
+        document.body.innerHTML = object.id
+        console.log(object)
+    })
+        
+    .catch(error => {
+        document.body.innerHTML = error.message
+        console.log(error)
+    
+    })
+    
+};
+
+submitData()
